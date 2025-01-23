38da276d28747c5b76654011aca57f236cf7b069
sivakumar <sivakumarec99@gmail.com>
2025-01-23 14:46:35 +0530

    Update ViewController.swift
    Loding added in main view

--- appdev/appdev/ViewController.swift
+++ appdev/appdev/ViewController.swift
@@ -12,8 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        indicator()
     }
 
+    func indicator(){
+        // Show loading indicator with a custom message
+        LoadingIndicator.shared.show(on: self.view, message: "Fetching data...")
+        
+        // Simulate a delay and hide loading indicator
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            LoadingIndicator.shared.hide()
+        }
+    }
 
+     
 }
 
